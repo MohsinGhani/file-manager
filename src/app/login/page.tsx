86be953044bc6086675d1a6b0b2e87caab1e63de
mohsin ghani <mohsinghani.777@gmail.com>
2023-10-11 15:39:54 -0700

    working on verification

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,152 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { Button, Checkbox, Form, Input, Modal } from "antd";
+
+import { auth } from "../../../firebase";
+import { CloseOutlined } from "@ant-design/icons";
+import { useAuthContext } from "../layout";
 
 const Page = () => {
-  return <div>page</div>;
-};
+  const [form] = Form.useForm();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
+  const onFinish = async (values: any) => {
+    const { email, password } = values;
+
+    setLoading(true);
+
+    form
+      .validateFields()
+      .then(() => {
+        signInWithEmailAndPassword(auth, email, password)
+          .then((res) => {
+            if (res.user.emailVerified === true) {
+              router.push("/dashboard");
+            } else {
+              Modal.error({
+                title: "Please verify your email first",
+                content: "Please check your email and verify your account.",
+                okText: "Close",
+                closeIcon: <CloseOutlined />,
+                onOk: () => {},
+                className: "custom-success-modal",
+              });
+              router.push("/dashboard");
+            }
+            console.log("res", auth.currentUser);
+          })
+          .catch((err) => {
+            if (err.code === "auth/invalid-login-credentials") {
+              form.setFields([
+                {
+                  name: "password",
+                  errors: ["Incorrect email or password please try again"],
+                },
+              ]);
+            } else {
+              form.setFields([
+                {
+                  name: "email",
+                  errors: [err?.code],
+                },
+              ]);
+            }
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      })
+      .catch((err:any) => {
+        console.log("Validation failed:", err);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="mx-auto max-w-[460px] mt-[50px] pl-[10px] pr-[10px]  items-center flex flex-col leading-[70px]">
+      <h1 className="font-[600] text-[30px] font-poppins text-[#000] mb-[-30px]">
+        Sign-in
+      </h1>
+      <p className="font-[500] text-[14px] font-poppins text-[#8591A3]  mb-[45px]"></p>
+
+      <Form form={form} onFinish={onFinish} className="max-w-[450px] w-full">
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your email!",
+            },
+            {
+              type: "email",
+              message: "Please enter a valid email address!",
+            },
+          ]}
+        >
+          <Input
+            autoComplete="email"
+            placeholder="Email"
+            className="font-[500] text-[14px]  rounded-[10px] font-poppins text-[#8591A3] h-[60px] max-w-[450px] mb-[20px] "
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your password!",
+            },
+          ]}
+        >
+          <Input.Password
+            autoComplete="current-password"
+            placeholder="Password"
+            className="h-[60px] max-w-[450px] rounded-[10px]"
+            maxLength={16}
+          />
+        </Form.Item>
+
+        <div className="flex justify-between">
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className="mr-[5px] cursor-pointer items-center flex text-[#b82f2f]   font-poppins text-[16px] font-[500]">
+              Remember me
+            </Checkbox>
+          </Form.Item>
+          <p
+            onClick={() => {
+              router.push("/forgot-password");
+            }}
+            className="text-[#b82f2f]  font-poppins text-[16px] font-[500] cursor-pointer"
+          >
+            Forgot password?
+          </p>
+        </div>
+        <Form.Item>
+          <Button
+            htmlType="submit"
+            loading={loading}
+            className="rounded-[10px] bg-[#790909]  max-w-[450px] w-full h-[50px]  login text-white text-[18px] font-[500]  font-poppins"
+          >
+            Sign in
+          </Button>
+        </Form.Item>
+      </Form>
+
+      <Button
+        onClick={() => {
+          router.push("/signup");
+        }}
+        className="rounded-[10px] bg-[#790909]  max-w-[450px] w-full h-[50px]  login text-white text-[18px] font-[500]  font-poppins"
+      >
+        Sign up
+      </Button>
+    </div>
+  );
+};
 export default Page;
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,236 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
+
+import { auth, db } from "../../../firebase";
+import { Modal } from "antd";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { Button, Form, Input } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
+import { doc, setDoc } from "firebase/firestore";
+import PhoneInput from "react-phone-input-2";
+import { useAuthContext } from "../layout";
 
 const Page = () => {
-  return <div>page</div>;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [number, setNumber] = useState("");
+  const router = useRouter() // Updated import
+const {setUpRecaptcha} = useAuthContext()
+
+  const onFinish = async (values: any) => {
+    console.log("🚀 ~ values:", values);
+
+    const {
+      firstName,
+      lastName,
+      email,
+
+      phoneNumber,
+      password,
+      confirmPassword,
+    } = values;
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match!");
+      return;
+    }
+   
+     
+      if (number === "" || number === undefined)
+        return setError("Enter number");
+      try {
+        const resonsne = await setUpRecaptcha(number);
+        console.log(resonsne);
+      } catch (err) {
+        console.log(err);
+      }
+    
+    // try {
+    //   setLoading(true);
+
+    //   // const authUser = await createUserWithEmailAndPassword(
+    //   //   auth,
+    //   //   email,
+    //   //   password
+    //   // );
+
+    //   const firstName = values.firstName;
+
+    //   await updateProfile(authUser.user, { displayName: firstName });
+    //   await sendEmailVerification(authUser.user);
+
+    //   const uid = authUser.user.uid;
+    //   await setDoc(doc(db, "Users", uid), {
+    //     firstName,
+    //     lastName,
+    //     email,
+
+    //     phone: phoneNumber,
+    //   });
+
+    //   setLoading(false);
+    //   getOtp(number);
+    //   Modal.success({
+    //     title: "Sign up successful!",
+    //     content: "Please check your email and verify your account.",
+    //     okText: "Close",
+    //     closeIcon: <CloseOutlined />,
+    //     onOk: () => {
+    //       router.push("/sign-in");
+    //     },
+    //     className: "custom-success-modal",
+    //   });
+    // } catch (err) {
+    //   console.error("Error:", err);
+    //   setLoading(false);
+    //   setError("An error occurred during sign up.");
+    // }
+    
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.error("Validation failed:", errorInfo);
+  };
+  return (
+    <div className="w-full bg relative h-[85vh] pl-[10px] pr-[10px] bg-[#000]">
+      <div className="mx-auto max-w-[460px] mt-[50px] items-center flex flex-col">
+        <h1 className="font-[600] text-[30px] font-poppins text-[#000] mb-[-30px]">
+          Signup to thread
+        </h1>
+        <p className="font-[500] text-[14px] font-poppins text-[#8591A3] mb-[45px]"></p>
+        <Form
+          name="signup"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          initialValues={{
+            // firstName: "",
+            // lastName: "",
+            // email: "",
+            phoneNumber: "",
+            // password: "",
+            // confirmPassword: "",
+          }}
+        >
+          <div className="max-w-[1280px] justify-between gap-[10px] w-full flex max-sm:flex-col mb-[20px]">
+          {/* <Form.Item
+              name="firstName"
+              rules={[
+                { required: true, message: "Please enter your first name." },
+              ]}
+            >
+              <Input
+                className="font-[500] text-[14px]  w-[225px]    max-sm:w-[100%] rounded-[10px] font-poppins text-[#8591A3] h-[60px] "
+                placeholder="First Name"
+                maxLength={16}
+              />
+            </Form.Item>
+            <Form.Item
+              name="lastName"
+              rules={[
+                { required: true, message: "Please enter your last name." },
+              ]}
+            >
+              <Input
+                className="font-[500] text-[14px] w-[225px]  max-sm:w-[100%]  rounded-[10px] font-poppins text-[#8591A3] h-[60px] "
+                placeholder="Last Name"
+                maxLength={16}
+              />
+            </Form.Item>
+        
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: "Please enter your email address." },
+              { type: "email", message: "Please enter a valid email address." },
+            ]}
+          >
+            <Input
+              className="font-[500] text-[14px]  rounded-[10px] font-poppins text-[#8591A3] h-[60px] mb-[20px] "
+              placeholder="Email"
+            />
+          </Form.Item> */}
+            </div>
+
+          <Form.Item
+            name="phoneNumber"
+            rules={[
+              { required: true, message: "Please enter your phone number" },
+            ]}
+          >
+            <PhoneInput
+              country={"pak"}
+              value={number}
+              onChange={setNumber}
+              placeholder="Enter phone number"
+            />
+          </Form.Item>
+
+          {/* <Form.Item
+            name="password"
+            rules={[
+              { required: true, message: "Please enter a password" },
+              { min: 6, message: "Password must be at least 6 characters" },
+            ]}
+          >
+             <Input.Password
+              className="font-[500] text-[14px]  rounded-[10px] font-poppins text-[#8591A3] h-[60px]  mb-[20px] "
+              placeholder="Password"
+              maxLength={16}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="confirmPassword"
+            rules={[
+              { required: true, message: "Please confirm your password" },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value.trim()) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject("Passwords do not match!");
+                },
+              }),
+            ]}
+          >
+           <Input.Password
+              className="font-[500] text-[14px]  rounded-[10px] font-poppins text-[#8591A3] h-[60px]  mb-[20px] "
+              placeholder="Confirm Password"
+              maxLength={16}
+            />
+          </Form.Item> */}
+
+          {/* ... Terms and conditions ... */}
+          <Form.Item>
+            <Button
+              htmlType="submit"
+              className="rounded-[10px] bg-[#790909] max-w-[460px] w-full h-[50px] login text-white text-[18px] font-[500] font-poppins"
+            >
+              Sign up
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <p className="text-center mt-[10px]">
+          Already have an account?{" "}
+          <a
+            onClick={() => {
+              router.push("/");
+            }}
+            className="rounded-[10px] max-w-[450px] w-full h-[50px] cursor-pointer text-[#b82f2f] text-[18px] font-[500] hover:underline"
+          >
+            Sign in
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
